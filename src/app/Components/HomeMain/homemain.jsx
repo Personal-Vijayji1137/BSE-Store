454bfa492ebc66e2e9e0070af3a1596a30d21a6d
jitenderji1137 <trademetrader1137@gmail.com>
2023-10-21 22:54:48 +0530

    Main

--- src/app/Components/HomeMain/homemain.jsx
+++ src/app/Components/HomeMain/homemain.jsx
@@ -1,23 +1,92 @@
 import Styles from "./homemain.module.css"
 import Image from "next/image"
+import Link from "next/link"
 export default function HomeMain(){
     return(
         <>
-           <main className={Styles.Main}>
-            <div className={Styles.DivOne}>
-                <h1><span>Web Versatility</span><span>Hub</span></h1>
-                <p>Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning</p>
-                <div className={Styles.LogInp}>
-                    <input className={Styles.Input} required="" type="email" placeholder="Your Email ID"/>
-                    <button className={Styles.Btn}>Get Start</button>
+            <main className={Styles.Main}>
+                <div className={Styles.DivOne}>
+                    <h1><span>Web Versatility</span><span>Hub</span></h1>
+                    <p>Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning</p>
+                    <div className={Styles.LogInp}>
+                        <input className={Styles.Input} required="" type="email" placeholder="Your Email ID"/>
+                        <button className={Styles.Btn}>Get Start</button>
+                    </div>
                 </div>
-            </div>
-            <div className={Styles.DivTwo}>
-                <Image className={Styles.BannerImage} src="/HomeBanner.png" width={1000} height={1000}/>
-            </div>
+                <div className={Styles.DivTwo}>
+                    <Image className={Styles.BannerImage} src="/HomeBanner.png" width={1000} height={1000}/>
+                </div>
            </main>
            <div className={Styles.BottomBanner}>
                <img src="/BottomBanner.svg"/>
+           </div>
+           <div className={Styles.AboutBSE}>
+            <div>
+                <div>
+                    <div>
+                        <Image src="/B-Red.png" width={500} height={500}/>
+                        <Image src="/BB-Red.svg" width={500} height={500}/>
+                    </div>
+                </div>
+                <div>
+                    <div>(B) Blogging</div>
+                    <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design.</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <Image src="/G-Green.png" width={500} height={500}/>
+                        <Image src="/GG-Green.svg" width={500} height={500}/>
+                    </div>
+                </div>
+                <div>
+                    <div>(S) Shopping</div>
+                    <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design.</p>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <Image src="/Y-Yellow.png" width={500} height={500}/>
+                        <Image src="/YY-Yellow.svg" width={500} height={500}/>
+                    </div>
+                </div>
+                <div>
+                    <div>(E) Entertainment</div>
+                    <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design.</p>
+                </div>
+            </div>
+           </div>
+           <div className={Styles.OurBlog}>
+                <div>Our Blogs</div>
+                <Link href="/blogs" className={Styles.Btn}>All Articles</Link>
+            </div>
+           <div className={Styles.OurBlogs}>
+            <div>
+                <div>
+                    <Image src="https://i.ytimg.com/vi/qXeT4KARlkU/maxresdefault.jpg" width={500} height={500}/>
+                </div>
+                <div>This is heading to one line and it can be more...</div>
+                <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design.</p>
+                <button className={Styles.ReadMoreBtn}>Read More &#x2794;</button>
+            </div>
+            <div>
+                <div>
+                    <Image src="https://i.ytimg.com/vi/qXeT4KARlkU/maxresdefault.jpg" width={500} height={500}/>
+                </div>
+                <div>This is heading to one line and it can be more...</div>
+                <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design.</p>
+                <button className={Styles.ReadMoreBtn}>Read More &#x2794;</button>
+            </div>
+            <div>
+                <div>
+                    <Image src="https://i.ytimg.com/vi/qXeT4KARlkU/maxresdefault.jpg" width={500} height={500}/>
+                </div>
+                <div>This is heading to one line and it can be more...</div>
+                <p>Most common methods for designing websites that work well on desktop is responsive and adaptive design.</p>
+                <button className={Styles.ReadMoreBtn}>Read More &#x2794;</button>
+            </div>
            </div>
         </>
     )
