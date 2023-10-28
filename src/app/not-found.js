08baa0375d8625dadb76a241773dae386605c51e
jitenderji1137 <trademetrader1137@gmail.com>
2023-10-28 20:28:31 +0530

    first commit

---
+++ src/app/not-found.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import "./Components/not-found.css"
+export default function NotFound() {
+  return (
+    <>
+    <div className="Not">
+        <div>
+            <div>This page has been abducted [404]</div>
+            <p>Choose your path below. The truth is out there…</p>
+            <Link href="/">Home Page</Link>
+        </div>
+        <div>
+            <Image src="https://i.postimg.cc/K8PSDFQb/ufo-2x.png" width={500} height={500} alt=''/>
+        </div>
+    </div>
+    </>
+  )
+}
