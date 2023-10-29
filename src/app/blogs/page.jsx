000114612408bb578f89bce9e212fd5c3f482941
jitenderji1137 <trademetrader1137@gmail.com>
2023-10-29 16:04:10 +0530

    first commit

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,6 +1,8 @@
+import BlogMainBanner from "./Components/BlogMainBanner.jsx/BlogMainBanner"
 export default function Blogs(){
     return(
         <>
+        <BlogMainBanner/>
         </>
     )
 }
