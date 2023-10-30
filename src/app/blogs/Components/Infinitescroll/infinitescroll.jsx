8acc8b085fbb084ecd77c1680380c581293fda0b
jitenderji1137 <trademetrader1137@gmail.com>
2023-10-30 20:12:11 +0530

    first commit

--- src/app/blogs/Components/Infinitescroll/infinitescroll.jsx
+++ src/app/blogs/Components/Infinitescroll/infinitescroll.jsx
@@ -1,20 +1,53 @@
 import Styles from "./infinitescroll.module.css"
+import Link from "next/link"
 export default function InfiniteScroll(){
     return(
         <>
-        <div className={Styles.InfiniteScroll}>
-            <ul>
-                <div>one</div>
-                <div>two</div>
-                <div>three</div>
-                <div>four</div>
-                <div>five</div>
-                <div>six</div>
-                <div>seven</div>
-                <div>eight</div>
-                <div>nine</div>
-                <div>ten</div>
-            </ul>
+        <div className={Styles.scrollingcontainer}> 
+            <div className={Styles.scrollingcontent}>
+                <Link href="/">one</Link>
+                <Link href="/">two</Link>
+                <Link href="/">three</Link>
+                <Link href="/">four</Link>
+                <Link href="/">five</Link>
+                <Link href="/">six</Link>
+                <Link href="/">seven</Link>
+                <Link href="/">eight</Link>
+                <Link href="/">nine</Link>
+                <Link href="/">ten</Link>
+                <Link href="/">one</Link>
+                <Link href="/">two</Link>
+                <Link href="/">three</Link>
+                <Link href="/">four</Link>
+                <Link href="/">five</Link>
+                <Link href="/">six</Link>
+                <Link href="/">seven</Link>
+                <Link href="/">eight</Link>
+                <Link href="/">nine</Link>
+                <Link href="/">ten</Link>
+            </div>
+            <div className={Styles.scrollingcontent}>
+                <Link href="/">one</Link>
+                <Link href="/">two</Link>
+                <Link href="/">three</Link>
+                <Link href="/">four</Link>
+                <Link href="/">five</Link>
+                <Link href="/">six</Link>
+                <Link href="/">seven</Link>
+                <Link href="/">eight</Link>
+                <Link href="/">nine</Link>
+                <Link href="/">ten</Link>
+                <Link href="/">one</Link>
+                <Link href="/">two</Link>
+                <Link href="/">three</Link>
+                <Link href="/">four</Link>
+                <Link href="/">five</Link>
+                <Link href="/">six</Link>
+                <Link href="/">seven</Link>
+                <Link href="/">eight</Link>
+                <Link href="/">nine</Link>
+                <Link href="/">ten</Link>
+            </div>
         </div>
         </>
     )
