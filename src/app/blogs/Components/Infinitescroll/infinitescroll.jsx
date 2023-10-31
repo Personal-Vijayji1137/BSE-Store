2752212adadebc5a70d03a4adfcc21583486e029
jitenderji1137 <trademetrader1137@gmail.com>
2023-10-31 16:32:14 +0530

    first commit

--- src/app/blogs/Components/Infinitescroll/infinitescroll.jsx
+++ src/app/blogs/Components/Infinitescroll/infinitescroll.jsx
@@ -3,51 +3,22 @@ import Link from "next/link"
 export default function InfiniteScroll(){
     return(
         <>
-        <div className={Styles.scrollingcontainer}> 
-            <div className={Styles.scrollingcontent}>
-                <Link href="/">one</Link>
-                <Link href="/">two</Link>
-                <Link href="/">three</Link>
-                <Link href="/">four</Link>
-                <Link href="/">five</Link>
-                <Link href="/">six</Link>
-                <Link href="/">seven</Link>
-                <Link href="/">eight</Link>
-                <Link href="/">nine</Link>
-                <Link href="/">ten</Link>
-                <Link href="/">one</Link>
-                <Link href="/">two</Link>
-                <Link href="/">three</Link>
-                <Link href="/">four</Link>
-                <Link href="/">five</Link>
-                <Link href="/">six</Link>
-                <Link href="/">seven</Link>
-                <Link href="/">eight</Link>
-                <Link href="/">nine</Link>
-                <Link href="/">ten</Link>
-            </div>
-            <div className={Styles.scrollingcontent}>
-                <Link href="/">one</Link>
-                <Link href="/">two</Link>
-                <Link href="/">three</Link>
-                <Link href="/">four</Link>
-                <Link href="/">five</Link>
-                <Link href="/">six</Link>
-                <Link href="/">seven</Link>
-                <Link href="/">eight</Link>
-                <Link href="/">nine</Link>
-                <Link href="/">ten</Link>
-                <Link href="/">one</Link>
-                <Link href="/">two</Link>
-                <Link href="/">three</Link>
-                <Link href="/">four</Link>
-                <Link href="/">five</Link>
-                <Link href="/">six</Link>
-                <Link href="/">seven</Link>
-                <Link href="/">eight</Link>
-                <Link href="/">nine</Link>
-                <Link href="/">ten</Link>
-            </div>
+        <div className={Styles.Tags}>BSE-Tags</div>
+        <div className={Styles.scrollingcontainer}>
+            {Array.from({ length: 3 }, (_, inde) => (
+                <div className={Styles.scrollingcontent} key={inde}>
+                    <Link href="/"><span></span><span>One</span></Link>
+                    <Link href="/"><span></span><span>two</span></Link>
+                    <Link href="/"><span></span><span>three</span></Link>
+                    <Link href="/"><span></span><span>four</span></Link>
+                    <Link href="/"><span></span><span>five</span></Link>
+                    <Link href="/"><span></span><span>six</span></Link>
+                    <Link href="/"><span></span><span>seven</span></Link>
+                    <Link href="/"><span></span><span>eight</span></Link>
+                    <Link href="/"><span></span><span>nine</span></Link>
+                    <Link href="/"><span></span><span>ten</span></Link>
+                </div>
+            ))}
         </div>
         </>
     )
---
+++ src/app/blogs/Components/Recentpost/recentpost.jsx
@@ -0,0 +1,11 @@
+import Styles from "./RecentPost.module.css"
+export default function RecentPost(){
+    return(
+        <>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        </>
+    )
+}
