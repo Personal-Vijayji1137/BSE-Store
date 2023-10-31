c6bd5c123e38adcd31a1709cab693a10c336d2d1
jitenderji1137 <trademetrader1137@gmail.com>
2023-10-31 17:04:34 +0530

    first commit

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -1,6 +1,6 @@
 import BlogMainBanner from "./Components/BlogMainBanner.jsx/BlogMainBanner"
 import InfiniteScroll from "./Components/Infinitescroll/infinitescroll"
-import RecentPost from "./Components/Recentpost/RecentPost"
+import RecentPost from "./Components/Recentpost/recentpost"
 export default function Blogs(){
     return(
         <>
