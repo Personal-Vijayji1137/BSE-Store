f85f0f77b8487846b6d85ed9599093b12d67c119
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-03 22:58:25 +0530

    first commit

--- src/app/blogs/post/[title]/Components/AboutPost/aboutpost.jsx
+++ src/app/blogs/post/[title]/Components/AboutPost/aboutpost.jsx
@@ -12,13 +12,13 @@ export default function AboutPost(){
                 <div><Link href="/"><span></span><span>Earning</span></Link></div>
         </div>
         <div className={Styles.Heading}>
-            <h1>All the money in the world can't buy you back good health</h1>
+            <h1>All the money in the world can&apos;t buy you back good health</h1>
         </div>
         <div className={Styles.Author}>By Jitender&nbsp;&nbsp;&nbsp;|| &nbsp;&nbsp;&nbsp;May 2, 2022&nbsp;&nbsp;&nbsp;|| &nbsp;&nbsp;&nbsp; 4 min read</div>
         <p className={Styles.Paragraph}>She then expatiated very warmly upon the advantages I should reap from her plan talked in a high style of my future grandeur assured me how heartily I should despise almost every body and every thing I had hitherto seen predicted my marrying into some family of the first rank in the kingdom and finally said I should spend a few months in Paris where my education and manners might receive their last polish.</p>
         <p className={Styles.Paragraph}>She then expatiated very warmly upon the advantages I should reap from her plan talked in a high style of my future grandeur assured me how heartily I should despise almost every body and every thing I had hitherto seen predicted my marrying into some family of the first rank in the kingdom and finally said I should spend a few months in Paris where my education and manners might receive their last polish.</p>
         <div className={Styles.SubHeading}>
-            <h2>1. All the money in the world can't buy you back good health</h2>
+            <h2>1. All the money in the world can&apos;t buy you back good health</h2>
         </div>
         <p className={Styles.Paragraph}>She then expatiated very warmly upon the advantages I should reap from her plan talked in a high style of my future grandeur assured me how heartily I should despise almost every body and every thing I had hitherto seen predicted my marrying into some family of the first rank in the kingdom and finally said I should spend a few months in Paris where my education and manners might receive their last polish.</p>
         <div className={Styles.Link}>
