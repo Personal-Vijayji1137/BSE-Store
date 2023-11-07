447ac191aff69f975af3b1cbeddcf9f371858ea4
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-07 21:38:54 +0530

    first commit

--- src/app/entertainment/page.jsx
+++ src/app/entertainment/page.jsx
@@ -2,7 +2,6 @@
 import GetMoviesList from "../Server/Movies/GetMoviesList" 
 export default async function Entertainment(){
   const Data = await GetMoviesList;
-  console.log(Data.data.data.listBSEMovies);
   return (
     <>
     <div></div>
