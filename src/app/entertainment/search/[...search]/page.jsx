5d7f6eb62a03311a9cef6598da13c337dcbf0fac
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-11 14:44:23 +0530

    first commit

--- src/app/entertainment/search/[...search]/page.jsx
+++ src/app/entertainment/search/[...search]/page.jsx
@@ -1,6 +1,38 @@
-export default async function Search({ params }){
+'use client'
+import { useState, useEffect } from "react"
+import Styles from "./search.module.css"
+import SearchResult from "./queryresult"
+import Image from "next/image"
+import Link from "next/link"
+export default function Search({ params }){
+    const [query,queryvalue] = useState(params.search[0]);
+    const [page,pagevalue] = useState(1);
+    const [Data,datavalue] = useState([]);
+    async function FetchData(){
+        const data = await SearchResult(query);
+        datavalue(data);
+    }
+    useEffect(()=>{
+        FetchData();
+    },[query])
     return(
         <>
+        <div className={Styles.Banners}>
+            <div>
+            <form className={Styles.BlogSearch}>
+                    <input type="text" value={query} placeholder="Search..." onChange={(e)=>{queryvalue(e.target.value)}} />
+                </form>
+                <div className={Styles.Results}>Results</div>
+                {Data.map((item)=>{
+                    return <Link href={`/blogs/post/${item.ID}/${item.Title.split(" ").join("-")}`} key={item.ID} className={Styles.SinglePost}>
+                        <div><Image src={item.Image} width={400} height={400} alt=""/></div>
+                        <div>
+                            <div>{item.Title}</div>
+                        </div>
+                    </Link>
+                })}
+            </div>
+        </div>
         </>
     )
 }
