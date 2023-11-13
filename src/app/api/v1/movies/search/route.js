81c5ff683df863d119a4717dfc3f13fa259d4e92
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-13 08:19:28 +0530

    first commit

--- src/app/api/v1/movies/search/route.js
+++ src/app/api/v1/movies/search/route.js
@@ -10,4 +10,19 @@ export async function POST(request) {
 export async function GET() {
     const data = {"message":"GET or any other Method is not alowed, Please use POST Method only ..."}
     return Response.json(data);
+}export async function PATCH() {
+    const data = {"message":"PATCH or any other Method is not alowed, Please use POST Method only ..."}
+    return Response.json(data);
+}export async function PUT() {
+    const data = {"message":"PUT or any other Method is not alowed, Please use POST Method only ..."}
+    return Response.json(data);
+}export async function DELETE() {
+    const data = {"message":"DELETE or any other Method is not alowed, Please use POST Method only ..."}
+    return Response.json(data);
+}export async function HEAD() {
+    const data = {"message":"HEAD or any other Method is not alowed, Please use POST Method only ..."}
+    return Response.json(data);
+}export async function OPTIONS() {
+    const data = {"message":"OPTIONS or any other Method is not alowed, Please use POST Method only ..."}
+    return Response.json(data);
 }
