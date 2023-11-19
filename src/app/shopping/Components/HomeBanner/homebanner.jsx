cd7cd3fcdb3f831708bd6763666c867a828a08c8
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-19 12:03:41 +0530

    first commit

--- src/app/shopping/Components/HomeBanner/homebanner.jsx
+++ src/app/shopping/Components/HomeBanner/homebanner.jsx
@@ -1,13 +1,16 @@
 import Link from "next/link"
 import Image from "next/image"
+import Styles from "./homebanner.module.css"
 export default function HomeBanner(){
     return(
         <>
-        <div>
+        <div style={{ backgroundImage: `url(/ShoppingHomeBG.svg)`}} className={Styles.HomeBanner}>
             <div>
-                <div>In this season, find the best</div>
-                <div>Exclusive collection for everyone</div>
-                <Link href="/">Explore now</Link>
+                <div>
+                    <div><span>🔥</span> In this season, find the best <span>🔥</span></div>
+                    <div>Exclusive collection for everyone</div>
+                    <Link href="/">Explore now</Link>
+                </div>
             </div>
             <div><Image src="/ShoppingBanner.png" width={1000} height={1000}/></div>
         </div>
