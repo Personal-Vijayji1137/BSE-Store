9b42982456ced8239f22cf95ce77355cfb6e16aa
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-21 08:53:44 +0530

    first commit

--- src/app/ads/banner.js
+++ src/app/ads/banner.js
@@ -3,6 +3,6 @@ import Script from "next/script";
 import Styles from "./directlink.module.css"
 export default async function AdsBanner(){
   return <><div className={Styles.EmbedLink} id="container-4017ddd058fb7d000a78b4429e3eca1a"></div>
-  <Script data-cfasync="false" src="//playgroundordinarilymess.com/4017ddd058fb7d000a78b4429e3eca1a/invoke.js"></Script>
+  {/* <Script data-cfasync="false" src="//playgroundordinarilymess.com/4017ddd058fb7d000a78b4429e3eca1a/invoke.js"></Script> */}
   </>
 };
