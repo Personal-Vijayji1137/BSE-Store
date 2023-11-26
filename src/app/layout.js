4e915377206adc51a664a9642930f3f41472228c
jitenderji1137 <trademetrader1137@gmail.com>
2023-11-26 12:06:01 +0530

    BSE

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,22 +7,6 @@ import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   metadataBase: new URL('https://bsestore.vercel.app/'),
-  title: 'BSE - Store',
-  description: 'Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning',
-  openGraph: {
-    title: 'BSE - Store',
-    description: 'Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning',
-    images: 'https://i.postimg.cc/Z5nL2cdZ/Screenshot-97.png',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'BSE - Store',
-    description: 'Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning',
-    siteId: '1467726470533754880',
-    creator: '@nextjs',
-    creatorId: '1467726470533754880',
-    images: ['https://i.postimg.cc/Z5nL2cdZ/Screenshot-97.png'],
-  },
   generator: 'Next.js',
   applicationName: 'BSE - Store',
   referrer: 'origin-when-cross-origin',
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,22 @@
 import HomeMain from './Components/HomeMain/homemain'
+export const metadata = {
+  title: 'BSE - Store',
+  description: 'Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning',
+  openGraph: {
+    title: 'BSE - Store',
+    description: 'Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning',
+    images: 'https://i.postimg.cc/Z5nL2cdZ/Screenshot-97.png',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'BSE - Store',
+    description: 'Explore a diverse online platform offering shopping, blogging, movies, earning, and free courses. Your all-in-one destination for endless possibilities and learning',
+    siteId: '1467726470533754880',
+    creator: '@nextjs',
+    creatorId: '1467726470533754880',
+    images: ['https://i.postimg.cc/Z5nL2cdZ/Screenshot-97.png'],
+  }
+}
 export async function getStaticPaths() {
   return {props: {}}
 }
