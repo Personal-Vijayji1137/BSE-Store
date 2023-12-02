c1bc7e4cba661fc7dd0058f371c30fa34b3f0e9c
jitenderji1137 <trademetrader1137@gmail.com>
2023-12-02 11:55:56 +0530

    BSE

--- src/app/api/Components/movies/page.jsx
+++ src/app/api/Components/movies/page.jsx
@@ -1,10 +1,11 @@
 'use client'
 import Styles from "./moviespage.module.css"
+import copy from "copy-to-clipboard"
 export default function MoviesAPI(){
     return(
         <>
         <div className={Styles.MoviesAPITitle}>Get Home Banner Data</div>
-        <div className={Styles.MoviesAPIMethod}><span>POST</span><div className={Styles.MoviesAPIMethodDiv}>{process.env.NEXT_PUBLIC_API_BASE_URL}</div><div className={Styles.Path}>movies/banner</div></div>
+        <div className={Styles.MoviesAPIMethod}><span>POST</span><div className={Styles.MoviesAPIMethodDiv}>{process.env.NEXT_PUBLIC_API_BASE_URL}</div><div className={Styles.Path}>movies/banner</div><code className={Styles.Codetocopy} onClick={()=>{copy(`${process.env.NEXT_PUBLIC_API_BASE_URL}movies/banner`)}}>Copy</code></div>
         </>
     )
 }
