decd023f313f5624273df9162cdc935b2a10abcc
jitenderji1137 <trademetrader1137@gmail.com>
2023-12-08 09:03:41 +0530

    BSE

--- src/app/ads/DirectLink.jsx
+++ src/app/ads/DirectLink.jsx
@@ -3,7 +3,7 @@ export default function DirectLink(){
     return(
         <>
         <div className={Styles.EmbedLink}>
-            {/* <embed src="https://playgroundordinarilymess.com/w87yhs080q?key=005916bb020799a6a7fb36594e6acc66" type="" /> */}
+            <embed src="https://playgroundordinarilymess.com/w87yhs080q?key=005916bb020799a6a7fb36594e6acc66" type="" />
         </div>
         </>
     )
