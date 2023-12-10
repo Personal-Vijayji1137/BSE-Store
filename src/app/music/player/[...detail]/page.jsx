ed1609c118500e31631a46b8eadd541c63ddc9ba
jitenderji1137 <trademetrader1137@gmail.com>
2023-12-10 18:49:29 +0530

    BSE-Store

--- src/app/music/player/[...detail]/page.jsx
+++ src/app/music/player/[...detail]/page.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { FeaturedPlaylists } from "../../AllServerRequests"
 import Style from "./detail.module.css"
+import Link from "next/link";
 export async function generateMetadata({ params }) {
     return {
         title: `${params.detail[1]} on BSE-Store`,
@@ -26,19 +27,16 @@ export default async function MusicPlayer({ params }){
     const FeatureData = await FeaturedPlaylists('https://api.spotify.com/v1/browse/featured-playlists?limit=50&country=IN');
     return(
         <>
-            <div className={Style.MainDiv}>
                 <iframe className={Style.MusicPlayer} src={`https://open.spotify.com/embed/playlist/${params.detail[0]}?utm_source=generator&theme=0`} width="100%" height="552" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                 <div>
+                <div className={Style.viewgrid}>
                     {FeatureData.map((item)=>{
-                        return <div key={item.id} className={Style.RightMain}>
-                            <div>
-                                <Image src={item.image} width={100} height={100}/>
-                            </div>
-                            <div></div>
+                        return <div key={item.id}>
+                        <Link href={`/music/player/${item.id}/${item.name.split(" ").join("-")}/${item.image.split('/').join('---')}`}><Image className={Style.Images} src={item.image} alt={`Image ${item.id}`} title={item.name} width={200} height={200} /></Link>
                         </div>
                     })}
                 </div>
-            </div>
+                </div>
         </>
     )
 }
