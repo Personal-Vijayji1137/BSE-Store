ee0dc4fc41f7791044227860fb0a38dae165f13f
jitenderji1137 <trademetrader1137@gmail.com>
2023-12-10 12:02:43 +0530

    BSE-Store

--- src/app/music/page.jsx
+++ src/app/music/page.jsx
@@ -1,6 +1,7 @@
 export default function Music(){
     return(
         <>
+            <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/37i9dQZF1DX0XUfTFmNBRM?utm_source=generator&theme=0" width="100%" height="1000" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </>
     )
 }
